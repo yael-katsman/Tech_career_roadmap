7b12d7c4a031c183877055952ada2ac9f1747f69
yael-katsman <166139874+yael-katsman@users.noreply.github.com>
2024-04-19 17:50:44 +0300

    Update lab_user_simulation.py

--- lab_user_simulation.py
+++ lab_user_simulation.py
@@ -235,7 +235,6 @@ def run_user(url,junior, senior, expert):
   test_user_url = url
   test_user = merged_df[merged_df['url']==test_user_url]
   print("The user's profile:")
-  display(test_user[['skills','degree_field','courses_title','cert_titles']].head())
   user_recommendation(test_user,junior, senior, expert)
 
 if __name__ == "__main__":
