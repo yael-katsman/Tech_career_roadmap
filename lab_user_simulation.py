6b3c236210ffcbd5626c8441682dbcf83492224c
yael-katsman <166139874+yael-katsman@users.noreply.github.com>
2024-04-20 12:21:48 +0300

    Update lab_user_simulation.py

--- lab_user_simulation.py
+++ lab_user_simulation.py
@@ -250,10 +250,9 @@ def run_user(url,dataset):
   user_recommendation(test_user,dataset)
 
 if __name__ == "__main__":
-  if len(sys.argv) == 2:
+  if len(sys.argv) == 3:
     url = sys.argv[1]
     dataset = sys.argv[2]
     run_user(url, dataset)
-  print(len(sys.argv))
 
 
